1116c250127ddd8573e9e1e3a5aa7d4311bd52dc
Mike Levin <miklevin@gmail.com>
2026-09-16 14:26:45 -0400

    chore: Update targets file resolution strategy

--- prompt_foo.py
+++ prompt_foo.py
@@ -44,7 +44,7 @@ except ImportError:
 
 
 CONFIG_DIR = Path.home() / ".config" / "pipulate"
-TARGETS_FILE = CONFIG_DIR / "blogs.json"
+TARGETS_FILE = Path(os.environ.get("PIPULATE_BLOGS_CONFIG", CONFIG_DIR / "blogs.json")).expanduser()
 
 DEFAULT_TARGETS = {
     "1": {
